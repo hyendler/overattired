14f201663eccf3cd9508bd9f4e101abedc891a61
Lindsey Stevenson <ly.stevenson@gmail.com>
2016-02-19 11:35:39 -0800

    Update user.rb
    added polymorphic association again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_one :measurement, as: :measurable, dependent: :destory
   validates :first_name, :last_name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
 end
