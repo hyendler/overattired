f6609bf46278d3bb9d6fe6c05bf07d53d9d14b8b
iregina <reginawong14@gmail.com>
2016-02-19 15:55:25 -0800

    Fixed destory typo

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ActiveRecord::Base
-	has_one :measurement, as: :measurable, dependent: :destory
+	has_one :measurement, as: :measurable, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160219191623) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "measurements", force: :cascade do |t|
     t.string   "gender"
     t.integer  "hip"
@@ -54,7 +57,7 @@ ActiveRecord::Schema.define(version: 20160219191623) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
