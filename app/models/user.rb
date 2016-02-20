4c1bb00d6de05c354d37f49cab57b9ee5a87f801
hyendler <hyendler@gmail.com>
2016-02-19 20:56:58 -0800

    Start working on querying for algorithm

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,28 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   # before_action :authenticate_user!
+
+  
+
+  	# check if user male or female
+	# if female, get user's waist measurement
+	# 	query all products that are for female and have waist measurement that is the same or up to two inches above
+	# 	return those products
+	# else get user's chest measurement
+	# 	pull all products that are for male and have chest measurement that is the same or up to two inches above
+	# 	return those products
+
+	def match
+		if self.gender == "female"
+			waist = self.measurement.waist
+			Product.find(:all, :joins => "INNER JOIN products ON products.id = measurements.measurable.id", :conditions => ["waist >= ? AND waist <= ?", waist, waist + 2])
+		end
+		
+	end
 end
+
+
+
+			# Product.all(:conditions => "waist >= ? AND waist <= ?", waist, waist + 2)
