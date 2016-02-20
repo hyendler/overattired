17299e574d41bd9390ea20874d34c2adcdec6fe1
lindsey-s <ly.stevenson@gmail.com>
2016-02-20 14:47:42 -0800

    continue debugging user #update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    # returns @user
   end
 
   def update
+
+    if @user.update(user_params)
+      p @user.measurement
+      redirect_to user_path
+    else
+      errs
+      render 'edit'
+    end
+
     p "************************"
     p params
     p "************************"
@@ -16,26 +26,44 @@ class UsersController < ApplicationController
     p "************************"
     p params[:user][:measurement]
     p "************************"
+    # if @user.measurement
+    #   if @user.update(user_params)
+    #     p "************************"
+    #     p "in the first if condition"
+    #     p "************************"
+    #     redirect_to user_path
+    #   else
+    #     p "************************"
+    #     p "in the first else condition"
+    #     p "************************"
+    #     errs
+    #     render 'edit'
+    #   end
+    # else # user does not have a measurement
+    #     p "************************"
+    #     p "in the main else condition"
+    #     p "************************"
+    #   measurement = Measurement.new(params[:user][:measurement])
+    #   @user.measurement = measurement
 
-    if @user.measurement
-      if @user.update(user_params)
-        redirect_to user_path
-      else
-        errs
-        render 'edit'
-      end
-    else # user does not have a measurement
-      measurement = Measurement.new(params[:user][:measurement])
-      @user.measurement = measurement
-      if measurement.save!
-        if @user.update(user_params)
-          redirect_to user_path
-        else
-          errs
-          render 'edit'
-        end
-      end
-    end
+    #   if measurement.save!
+    #     p "************************"
+    #     p "in the measurement save condition"
+    #     p "************************"
+    #     if @user.update(user_params)
+    #       p "************************"
+    #       p "in the measurement user updated condition"
+    #       p "************************"
+    #       redirect_to user_path
+    #     else
+    #       p "************************"
+    #       p "in the measurement user update ELSE condition"
+    #       p "************************"
+    #       errs
+    #       render 'edit'
+    #     end
+    #   end
+    # end
   end
 
   private
@@ -44,7 +72,14 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :gender, measurement_attributes: [:id, :gender, :hip, :waist, :bust, :chest, :inseam])
+      # p "************************"
+      # p "USER_PARAMS METHOD"
+      # p "************************"
+      # p "PARAMS:"
+      # p "************************"
+      # p params
+      # p "************************"
+    params.require(:user).permit(:first_name, :last_name, :gender, measurement: [:id, :gender, :hip, :waist, :bust, :chest, :inseam])
   end
 
   def errs
