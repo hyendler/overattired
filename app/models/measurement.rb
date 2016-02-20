93b9d0510ca2cfc5436e9229a6de935519196257
hyendler <hyendler@gmail.com>
2016-02-19 18:41:12 -0800

    Run migration successfully, seeds are all working

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,3 +1,17 @@
 class Measurement < ActiveRecord::Base
 	belongs_to :measurable, polymorphic: true
+
+	# check for male or female
+	# if female, get user's waist measurement
+	# 	query all products that are for female and have waist measurement that is the same or up to two inches above
+	# 	return those products
+	# else get user's chest measurement
+	# 	pull all products that are for male and have chest measurement that is the same or up to two inches above
+	# 	return those products
+
+	def match
+		
+	end
+
+
 end
