dc569f5258727322160ea69ac56280cf1619899e
Lindsey Stevenson <ly.stevenson@gmail.com>
2016-02-19 22:01:05 -0800

    Update 20160219051242_create_measurements.rb
    updated indentations

--- db/migrate/20160219051242_create_measurements.rb
+++ db/migrate/20160219051242_create_measurements.rb
@@ -1,13 +1,13 @@
 class CreateMeasurements < ActiveRecord::Migration
   def change
     create_table :measurements do |t|
-        t.references :imageable, polymorphic: true, index: true
     	t.string :gender
     	t.float :hip
     	t.float :waist
     	t.float :bust
     	t.float :chest
     	t.float :inseam
+    	t.references :imageable, polymorphic: true, index: true
     	t.timestamps null: false
     end
   end
