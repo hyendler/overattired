d86c2563a2a1c71c192f4d8643af0ef5b3443353
lindsey-s <ly.stevenson@gmail.com>
2016-02-21 01:45:56 -0800

    correct edit measurement link on user show page

--- app/controllers/users/measurements_controller.rb
+++ app/controllers/users/measurements_controller.rb
@@ -1,2 +1,20 @@
 class Users::MeasurementsController < ApplicationController
+  before_action :set_measurement, only: [:show, :update, :destory]
+
+  def show
+    # returns @measurement
+  end
+
+  def edit
+    # returns @measurement
+  end
+
+  def update
+  end
+
+  private
+  def set_measurement
+    @measurement = Measurement.find(params[:id])
+  end
+
 end
