9c2889cf5aa1bd48628f68a9b04ae33dbe6f9ae8
Regina Wong <iregina@users.noreply.github.com>
2016-02-20 16:23:25 -0800

    Update 20160219050816_create_products.rb
    corrected spacing

--- db/migrate/20160219050816_create_products.rb
+++ db/migrate/20160219050816_create_products.rb
@@ -1,15 +1,15 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-    	t.string :name
-    	t.string :gender
-    	t.string :url
+      t.string :name
+      t.string :gender
+      t.string :url
       t.float :hip
       t.float :waist
       t.float :bust
       t.float :chest
       t.float :inseam
-    	t.timestamps null: false
+      t.timestamps null: false
     end
   end
 end
