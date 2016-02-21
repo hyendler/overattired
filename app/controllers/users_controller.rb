258c580dcb673bd5d8237cef69c0f7feb7769ee2
lindsey-s <ly.stevenson@gmail.com>
2016-02-20 17:27:53 -0800

    Implement destory user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,26 +9,20 @@ class UsersController < ApplicationController
     # returns @user
   end
 
-  # def new
-  #   super
-  #   # something
-  # end
-
   def update
-      if @user.update(user_params)
-        # p @user.measurement
-        redirect_to user_path
-      else
-        errs
-        render 'edit'
-      end
-    # p "************************"
-    # p params
-    # p "************************"
-    # p params[:user]
-    # p "************************"
-    # p params[:user][:measurement]
-    # p "************************"
+    if @user.update(user_params)
+      # p @user.measurement
+      redirect_to user_path
+    else
+      errs
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to "/"
+    # send a flash success
   end
 
   private
@@ -37,13 +31,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-      p "************************"
-      p "USER_PARAMS METHOD"
-      p "************************"
-      p "PARAMS:"
-      p "************************"
-      p params
-      p "************************"
     params.require(:user).permit(:first_name, :last_name, :gender, :hips, :waist, :bust, :chest, :inseam)
   end
 
