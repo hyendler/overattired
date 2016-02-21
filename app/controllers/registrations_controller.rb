3b4d8998f6931b65ffce64568e2c92b171dba692
hyendler <hyendler@gmail.com>
2016-02-20 18:20:07 -0800

    updated routes with measurements, updated user edit and show views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    edit_user_path(current_user)
+    # new_measurement_path
   end
 end
