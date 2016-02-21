52e9a1460eb3376ecfee1914f53b6d87bbce323d
iregina <reginawong14@gmail.com>
2016-02-21 12:44:46 -0800

    Add working user model spec for validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:form_no_first_name) {User.new(last_name: 'Calvo')}
-  let(:form_no_last_name)  {User.new(first_name: 'Ovi')}
-  let(:form)  {User.new(first_name: 'Ovi', last_name: 'Calvo')}
+  let(:form_no_first_name) {User.new(last_name: 'Calvo', email:'ovi@gmail.com', password: '12345678')}
+  let(:form_no_last_name)  {User.new(first_name: 'Ovi', email:'ovi@gmail.com', password: '12345678')}
+  let(:form_no_email)  {User.new(first_name: 'Ovi', last_name: 'Calvo', password: '12345678')}
+  let(:form_no_password)  {User.new(first_name: 'Ovi', last_name: 'Calvo', email: 'ovi@gmail.com')}
+  let(:form)  {User.new(first_name: 'Ovi', last_name: 'Calvo', email: 'ovi@gmail.com', password: '12345678')}
 
   describe 'validations' do
   	context 'will raise an error' do
   	  it 'when the first_name field is empty' do
   	    form_no_first_name.save
-  	    expect(form_no_first_name.errors[:first_name]).to
-  	      include("can't be blank")
+  	    expect(form_no_first_name.errors[:first_name]).to include("can't be blank")
   	  end
   	  it 'when the last_name field is empty' do
   	  	form_no_last_name.save
-  	  	expect(form_no_last_name).errors[:last_name]).to
-		  include("can't be blank")
+  	  	expect(form_no_last_name.errors[:last_name]).to include("can't be blank")
+	  end
+      it 'when the email field is empty' do
+  	  	form_no_email.save
+  	  	expect(form_no_email.errors[:email]).to include("can't be blank")
+	  end
+	  it 'when the password field is empty' do
+  	  	form_no_password.save
+  	  	expect(form_no_password.errors[:password]).to include("can't be blank")
 	  end
 	end
 	it 'saves the user when the fields are not blank' do
