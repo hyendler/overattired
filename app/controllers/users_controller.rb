0fd05ce2f02d62be93692034c09e0eeef785730a
iregina <reginawong14@gmail.com>
2016-02-21 21:08:16 -0800

    update delete method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def show
     # returns @user
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   let(:user2){User.create(first_name: "Hello", last_name: "Kitty", email: "hellokitty1@gmail.com", password: "12345678")}
+  let(:user3) { User.create(first_name: "Taylor", last_name: "Swift", email: "taytay@gmail.com", password: "12345678") }
 	  
 	  describe '#update' do
 	  	let(:first_name){"Pokemon"}
@@ -18,4 +19,24 @@ RSpec.describe UsersController, type: :controller do
         response.should redirect_to User.last
       end
     end
+
+	  describe 'DELETE #destroy' do
+
+	    it 'decrements users by one' do
+	    	user_id = user3.id
+	    	p user3
+	    	p user_id
+	    	p User.last
+	      put :destroy, id: user_id
+	      expect(User.last).not_to be(user3)
+	    end
+
+	    it 'redirects to the root path' do
+	      user_id = user3.id
+	      delete :destroy, id: user_id
+	      expect(response.status).to eq(302)
+	    end
+	  end
+
+
 end
