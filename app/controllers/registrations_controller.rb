f5ff0650ed5aae27be9d41bc75c6de2121b94132
iregina <reginawong14@gmail.com>
2016-02-21 22:06:15 -0800

    correct after_sign_in path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    new_user_measurement_path
+    new_user_measurement_path(current_user)
   end
 end
