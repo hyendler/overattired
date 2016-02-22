3b78c128b4ca2bbd36437b2de4fd70e28d43de1c
Lindsey Stevenson <ly.stevenson@gmail.com>
2016-02-22 11:41:24 -0800

    Update sessions_controller.rb
    resource.admin is a boolean - removed == true from line 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < Devise::SessionsController
 	def after_sign_in_path_for(resource)
-		if resource.admin == true
+		if resource.admin
 			admin_path
 		else
 			user_path(current_user.id)
