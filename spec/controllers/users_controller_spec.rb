d6e34411ef106c96b15edc7ca9b82987e541cac7
iregina <reginawong14@gmail.com>
2016-02-21 19:01:14 -0800

    test the update method for the user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,23 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-	let(:user1){User.create(first_name: "Lindsey", last_name: "Stevens", email: "lindsey@gmail.com", password: "12345678")}
+
   let(:user2){User.create(first_name: "Hello", last_name: "Kitty", email: "hellokitty1@gmail.com", password: "12345678")}
+	  
 	  describe '#update' do
- 
-      it 'updates an existing useron the server side' do
-      	p user2
-      	testing = User.last
-      	p testing
-        put :update, id: testing.id, first_name: "Pokemon", last_name: "Master"
-        p testing
-        expect(user3.first_name).to eq("Pokemon")
-        # expect(user3.last_name).to eq("Master")
+	  	let(:first_name){"Pokemon"}
+      let(:last_name){"Master"}
+      it 'updates an existing user on the server side' do
+        put :update, id: user2.id, user: { first_name: first_name, last_name: last_name }
+        expect(user2.reload.first_name).to eq("Pokemon")
+        expect(user2.reload.last_name).to eq("Master")
       end
 
-      # it 'redirects to the newly updated user' do
-      #   put :update, id: user3.id, first_name: "Pokemon", last_name: "Master"
-      #   response.should redirect_to User.last
-      # end
+      it 'redirects to the newly updated user' do
+        put :update, id: user2.id, user: { first_name: first_name, last_name: last_name }
+        response.should redirect_to User.last
+      end
     end
 end
