177275fd6e40579d299cfe0e784f9d9314e97e3c
iregina <reginawong14@gmail.com>
2016-02-23 11:54:57 -0800

    send users welcome email after signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
+  after_action :send_user_mail, only: [:create]
   protected
+
+  def send_user_mail
+    UserMailer.welcome_email(@user).deliver_now
+  end
 
   def after_sign_up_path_for(resource)
     new_user_measurement_path(current_user)
