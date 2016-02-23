504879a838ce60cbd9319b6fdb410d0fcfb6629c
iregina <reginawong14@gmail.com>
2016-02-22 20:17:23 -0800

    generate mailer and mailer view

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default from: 'letthewookiewin13@gmail.com'
+	 
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://overattired.com'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
