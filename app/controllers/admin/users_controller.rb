2a7333be909724796f79ebba522ab9356afa8fa9
iregina <reginawong14@gmail.com>
2016-02-23 20:10:43 -0800

    add loop in send_update method to email all users with matches

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,10 +7,18 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def send_updates
-  	@user = User.last
-  	MatchMailer.match_email(@user).deliver_now
+  	n = 6 # this is ID of the first user in your database
+  	2.times do # set this the number of users you want to go through
+	  	@user = User.find(n)
+	  	if @user.match == [] # is there are no matches don't send an email to this user
+	  		n +=1
+	  	else
+	  		MatchMailer.match_email(@user).deliver_now
+	  		n += 1
+	  	end
+  	end
   	flash[:notice] = "Matches have been sent."
-  	redirect_to admin_path
+	redirect_to admin_path
   end
 
 end
--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -2,8 +2,8 @@ class MatchMailer < ApplicationMailer
   default from: 'overattired1@gmail.com'
 
    def match_email(user)
-  	@user = User.last
-  		mail(to: "reginawong14@gmail.com", subject: 'Here are your matches!')
+    @user = user
+  	mail(to: @user.email, subject: "Heya #{@user.first_name}! Here are your matches!")
   end
   
 end
