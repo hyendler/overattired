35049bfa36d26309d3e46ef47dbf7ab12fa778c8
lindsey-s <ly.stevenson@gmail.com>
2016-02-24 00:18:10 -0800

    refactor etsy methods

--- lib/etsy.rb
+++ lib/etsy.rb
@@ -5,15 +5,6 @@ require 'json'
 #   p data["results"].length
 # end
 
-def scrape_etsy
-  # request all products (150 limit) in OverAttired Vintage Shop, Include Main Image url
-  url = URI("https://openapi.etsy.com/v2/shops/10849718/listings/active?includes=MainImage&limit=150&api_key=#{ENV["ETSY_KEYSTRING"]}")
-  data = Net::HTTP.get(url)
-  parsed_data = JSON(data)
-  # count_listings(parsed_data)
-  return parsed_data
-end
-
 #PARSING PRODUCT DATA
 def parse_title(listing)
   whole_title = listing["title"]
@@ -56,7 +47,6 @@ def parse_length(listing)
   end
 end
 
-
 def parse_sleeves(listing)
   description = listing["description"]
   if description.include?("Overall Length")
@@ -79,55 +69,78 @@ def parse_gender(listing)
   end
 end
 
+# request all products (200 max) in OverAttired Vintage Shop, Include Main Image url
+# save first url with 100 listing limit
+URL_1 = URI("https://openapi.etsy.com/v2/shops/10849718/listings/active?includes=MainImage&limit=100&api_key=#{ENV["ETSY_KEYSTRING"]}")
+# save second url with 100 listing limit and offset of 100 listings
+URL_2 = URI("https://openapi.etsy.com/v2/shops/10849718/listings/active?includes=MainImage&limit=100&offset=100&api_key=#{ENV["ETSY_KEYSTRING"]}")
+
+def scrape_etsy(url)
+  data = Net::HTTP.get(url)
+  parsed_data = JSON(data)
+  # count_listings(parsed_data)
+  return parsed_data
+end
+
+# CALL THIS METHOD IN CONTROLLER TO STORE NEW PRODUCTS
 def store_data_from_etsy
-  measurement = Measurement.new
-
-  parsed_data = scrape_etsy
-
+  # delete all products from database
   Product.delete_all
-
-  parsed_data["results"].each do |listing|
-    product = Product.new
-
-    title_array = parse_title(listing)
-
-    if title_array.length == 3
-      # store title, subtitle and size
-      product.title = parse_title(listing)[0].strip
-      product.subtitle = parse_title(listing)[1].strip
-      product.size = parse_title(listing)[2].strip
-    elsif title_array.length == 2
-      # store title and subtitle
-      product.title = parse_title(listing)[0].strip
-      product.subtitle = parse_title(listing)[1].strip
-    else
-      # store title only
-      product.title = parse_title(listing)[0].strip
-    end
-
-    product.price = listing["price"]
-    product.when_made = listing["when_made"]
-    product.description = description_parse(listing)
-    product.category = parse_category(listing)
-    product.url = listing["url"]
-    product.image_url = parse_image(listing)
-
-    if product.save
-      #add measurement
-      measurement = Measurement.new
-      measurement.bust = parse_measurement(listing, "Bust")
-      measurement.shoulders = parse_measurement(listing, "Shoulders")
-      measurement.waist = parse_measurement(listing, "Waist")
-      measurement.chest = parse_measurement(listing, "Chest")
-      measurement.inseam = parse_measurement(listing, "Inseam")
-      measurement.overall_length = parse_length(listing)
-      measurement.hips = parse_measurement(listing, "Hips")
-      measurement.sleeve_length = parse_sleeves(listing)
-      measurement.gender = parse_gender(listing)
-
-      if measurement.save
-        product.measurement = measurement
-      end
-    end
+  # call etsy API and store parse data into JSON
+  parsed_data_1 = scrape_etsy(URL_1)
+  # iterate through JSON and store product and measurement
+  parsed_data_1["results"].each do |listing|
+    save_product(listing)
   end
+  # repeat for second call to Etsy
+  parsed_data_2 = scrape_etsy(URL_2)
+  parsed_data_2["results"].each do |listing|
+    save_product(listing)
+  end
+end
+
+def save_product(listing)
+  product = Product.new
+  # save product title (possibly also subtitle and generic size)
+  title_array = parse_title(listing)
+  if title_array.length == 3
+    # store title, subtitle and size
+    product.title = parse_title(listing)[0].strip
+    product.subtitle = parse_title(listing)[1].strip
+    product.size = parse_title(listing)[2].strip
+  elsif title_array.length == 2
+    # store title and subtitle
+    product.title = parse_title(listing)[0].strip
+    product.subtitle = parse_title(listing)[1].strip
+  else
+    # store title only
+    product.title = parse_title(listing)[0].strip
+  end
+  # save additional attributes
+  product.price = listing["price"]
+  product.when_made = listing["when_made"]
+  product.description = description_parse(listing)
+  product.category = parse_category(listing)
+  product.url = listing["url"]
+  product.image_url = parse_image(listing)
+  # if the product saves, also save its measurement
+  if product.save
+    save_measurement(listing) ? product.measurement = save_measurement(listing) : false
+  end
+end
+
+def save_measurement(listing)
+  measurement = Measurement.new
+  # save measurement attributes
+  measurement.bust = parse_measurement(listing, "Bust")
+  measurement.shoulders = parse_measurement(listing, "Shoulders")
+  measurement.waist = parse_measurement(listing, "Waist")
+  measurement.chest = parse_measurement(listing, "Chest")
+  measurement.inseam = parse_measurement(listing, "Inseam")
+  measurement.overall_length = parse_length(listing)
+  measurement.hips = parse_measurement(listing, "Hips")
+  measurement.sleeve_length = parse_sleeves(listing)
+  measurement.gender = parse_gender(listing)
+  # if the measurement can save, return it so we can store it to the product in save_product
+  measurement.save ? measurement : false
 end
