81980d046775681ee6bd908ee297158db734b8f7
iregina <reginawong14@gmail.com>
2016-02-24 15:15:24 -0800

    add launchy gem and small changes to Mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,19 @@
 class UserMailer < ApplicationMailer
   default from: 'overattired1@gmail.com'
 
-  def welcome_email(user)
-  	@user = user
-  	@url = 'http://overattired.com'
-  	mail(to: @user.email, subject: 'Welcome to Over Attired!')
+  def contact_email(name, email, message)
+    @name = name
+    @email = email
+    @message = message
+    mail(from: @email,
+         to: 'reginawong14@gmail.com',
+         subject: 'Welcome E-mail!')
   end
+
+  # def welcome_email(user)
+  # 	@user = user
+  # 	@url = 'http://overattired.com'
+  # 	mail(to: @user.email, subject: 'Welcome to Over Attired!')
+  # end
 
 end
