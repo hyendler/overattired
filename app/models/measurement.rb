a5505feedca29a17b07c8b30c220046f339162cf
lindsey-s <ly.stevenson@gmail.com>
2016-02-24 20:10:41 -0800

    add conditional validations on Measurement model for gender

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -2,15 +2,20 @@ class Measurement < ActiveRecord::Base
 	belongs_to :measurable, polymorphic: true
 
   validates :gender, presence: true
+  validates :gender, :inclusion => {:in => %w(male female), :message => "Please select female or male"}
+
+  validates :hips, :bust, :waist, :shoulders, :presence => true, :if => :gender_is_female?
+
+  validates :waist, :shoulders, :chest, :inseam, :presence => true, :if => :gender_is_male?
+
   # validates :hips, :waist, :bust, :chest, :inseam, numericality: true
-  # validate :required_measurements, :measurement_is_a_number
 
-  # def required_measurements
-  #   if gender is male - if gender is female
-  # end
+  def gender_is_male?
+    self.gender == "male"
+  end
 
-  # def measurement_is_a_number
-  #   case
-  # end
+  def gender_is_female?
+    self.gender == "female"
+  end
 
 end
