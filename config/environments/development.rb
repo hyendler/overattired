94781f61e98fc117937508917c8f78187823a7e7
iregina <reginawong14@gmail.com>
2016-02-24 18:12:07 -0800

    Hunter becomes our lord and savior

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -33,15 +30,19 @@ Rails.application.configure do
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
-  # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
+
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
 
-  config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.delivery_method = :smtp
+    # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: "587",
