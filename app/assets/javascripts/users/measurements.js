742f631e3f5be9aaf693f15bf014ec6ea3f79596
Ovidio Calvo <OviWan@Ovidios-MacBook-Air.local>
2016-02-24 16:11:41 -0800

    Add initial javascript for user measurements and targeted div in user measurement form

--- app/assets/javascripts/users/measurements.js
+++ app/assets/javascripts/users/measurements.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+ +  $('#new_post_link').on('click', function(e){
+ +   console.log("this is just a test")
+ +   e.preventDefault()
+ +   $(this).toggle();
+ +   var targeted = $(this);
