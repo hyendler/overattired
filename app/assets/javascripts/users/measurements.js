464f3d54994b36bd6eca7a2e2b4098f57ed8b876
Ovidio Calvo <OviWan@Ovidios-MacBook-Air.local>
2016-02-24 18:34:45 -0800

    Add working JQuery to measurments form

--- app/assets/javascripts/users/measurements.js
+++ app/assets/javascripts/users/measurements.js
@@ -1,16 +1,46 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// $(document).ready(function() {
+//   measurementPicListener();
+// });
+
+
+// var measurementPicListener = function(){
+//   $('#measurement_hips').on('click', function(event){
+//   	event.preventDefault();
+
+//   	var target = $(this)
+
+//   	$('#measurement-image').append("<img src='/assets/images/hips.png'>")
+
+//   })
+  	
+
+// };
+
+
 $(document).ready(function() {
-  measurementPicListener();
+  bindListeners();
 });
 
 
-var measurementPicListener = function(){
-  $('#measurement_hips').on('click', function(event){
+var bindListeners = function(){
+	$('#measurement_hips').on('click', displayMeasurementPic)
+	$('#measurement_shoulders').on('click', displayMeasurementPic)
+	$('#measurement_inseam').on('click', displayMeasurementPic)
+	$('#measurement_bust').on('click', displayMeasurementPic)
+	$('#measurement_waist').on('click', displayMeasurementPic)
+	$('#measurement_chest').on('click', displayMeasurementPic)
+}
+
+var displayMeasurementPic = function(){
+ 
   	event.preventDefault();
   	var targeted = $(this);
+  	$(".measurement-image").hide();
+  	targeted.parent().children(".measurement-image").show();
 
-  	$('#measurement-image').append("<img src="/assets/images/hips.png">")
+  	// targeted.parent().append("<img src='/assets/images/hips.png'");
   	
 
 };
