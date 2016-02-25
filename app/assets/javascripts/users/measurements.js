662e4760b0e6aa6440788ff83479e20edf09b856
Ovidio Calvo <OviWan@Ovidios-MacBook-Air.local>
2016-02-24 17:21:00 -0800

    Add jquery

--- app/assets/javascripts/users/measurements.js
+++ app/assets/javascripts/users/measurements.js
@@ -1,8 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
- +  $('#new_post_link').on('click', function(e){
- +   console.log("this is just a test")
- +   e.preventDefault()
- +   $(this).toggle();
- +   var targeted = $(this);
+  measurementPicListener();
+});
+
+
+var measurementPicListener = function(){
+  $('#measurement_hips').on('click', function(event){
+  	event.preventDefault();
+  	var targeted = $(this);
+
+  	$('#measurement-image').append("<img src="/assets/images/hips.png">")
+  	
+
+};
