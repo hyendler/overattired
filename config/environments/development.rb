bce8b8c45915569c29b9df0060834dc2889c4cba
Regina Wong <iregina@users.noreply.github.com>
2016-02-25 17:37:34 -0800

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
     # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :letter_opener
+  
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
