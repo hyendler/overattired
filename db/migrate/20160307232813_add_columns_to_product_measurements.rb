2b0a43f3e54e6c7a1571987d72fdd1759c8f1cdd
lindsey-s <ly.stevenson@gmail.com>
2016-03-07 15:30:56 -0800

    get that migration son

--- db/migrate/20160307232813_add_columns_to_product_measurements.rb
+++ db/migrate/20160307232813_add_columns_to_product_measurements.rb
@@ -1,6 +1,6 @@
 class AddColumnsToProductMeasurements < ActiveRecord::Migration
   def change
-    change_table :measurements do |t|
+    change_table :product_measurements do |t|
       t.float :shoulders
       t.float :sleeve_length
       t.float :overall_length
