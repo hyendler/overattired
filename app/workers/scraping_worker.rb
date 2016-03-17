6d6b3bfed8ca6df2a3c5dbea4b130249a3f64fee
iregina <reginawong14@gmail.com>
2016-03-17 15:21:16 -0700

    create automatic scrapping

--- app/workers/scraping_worker.rb
+++ app/workers/scraping_worker.rb
@@ -2,10 +2,12 @@ class ScrapingWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence { daily.hour_of_day(1).minute_of_hour(55) }
+  # this is UTC
+  recurrence { daily.hour_of_day(4).minute_of_hour(43) }
 
 
   def perform
+  	# this just sends out something that says 
   	testing_method
     # implementation details...
   end
