6169358d4d8b783cfa3962d27c56bfdc04dc2ba2
hyendler <hyendler@gmail.com>
2016-03-17 11:03:54 -0700

    First attempt

--- app/controllers/user_measurements_controller.rb
+++ app/controllers/user_measurements_controller.rb
@@ -1,6 +1,6 @@
 class UserMeasurementsController < ApplicationController
   before_action :set_measurement, only: [:show, :edit, :update]
-  after_action :contact, only: [:create]
+  # after_action :contact, only: [:create]
 
   def show
     # returns @measurement
@@ -49,26 +49,26 @@ class UserMeasurementsController < ApplicationController
   # end
   # ----------------------------------------
 
-  def contact
-    # Look in the rails server - if you see all the stars it means that this method has started running
-    p '* ' * 1000
-    @user = User.find(params[:user_id])
-    # p "The contact method ran for #{@user.first_name}"
-    # don't really need 'name' here can remove it for refactoring
-    h = { 'name' => @user.first_name,
-          'email' => @user.email}
-    # just testing whether the inputs show up:
-    # p "THE INPUTS THAT GOES INTO THE EMAIL ARE:"
-    # p h
-    # p "before worker call"
-    PostmanWorker.perform_async(h, 5)
-    # NOW GO LOOK IN THE app/workers/postman_worker.rb
-    # If you see the follow text, it means this method has ran! Good job.
-    # p "After worker call"
-    # if instead of sidekiq I was just sending email from rails
-    # VisitorMailer.contact_email(@name, @email, @message).deliver
-    # redirect_to :root
-  end
+  # def contact
+  #   # Look in the rails server - if you see all the stars it means that this method has started running
+  #   p '* ' * 1000
+  #   @user = User.find(params[:user_id])
+  #   # p "The contact method ran for #{@user.first_name}"
+  #   # don't really need 'name' here can remove it for refactoring
+  #   h = { 'name' => @user.first_name,
+  #         'email' => @user.email}
+  #   # just testing whether the inputs show up:
+  #   # p "THE INPUTS THAT GOES INTO THE EMAIL ARE:"
+  #   # p h
+  #   # p "before worker call"
+  #   PostmanWorker.perform_async(h, 5)
+  #   # NOW GO LOOK IN THE app/workers/postman_worker.rb
+  #   # If you see the follow text, it means this method has ran! Good job.
+  #   # p "After worker call"
+  #   # if instead of sidekiq I was just sending email from rails
+  #   # VisitorMailer.contact_email(@name, @email, @message).deliver
+  #   # redirect_to :root
+  # end
 
   private
 
--- app/models/user_measurement.rb
+++ app/models/user_measurement.rb
@@ -1,3 +1,22 @@
 class UserMeasurement < ActiveRecord::Base
 	belongs_to :user
+	validates :gender, presence: true
+	validate :correct_gender_measurements
+
+	def correct_gender_measurements
+		if gender == "female"
+			if hips.blank? || bust.blank? || waist.blank?
+				errors.add(:hips, 'Attend to all measurements!')
+			end
+		elsif gender == "male"
+			if waist.blank? || chest.blank? || inseam.blank?
+				errors.add('Attend to all measurements!')
+			end
+		end
+	end
+	# validates :hips, presence :true, if "gender == 'female'"
+	# validates :bust
+	# validates :waist
+	# validates :chest
+	# validates :inseam
 end
